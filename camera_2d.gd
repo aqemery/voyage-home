f1566a5c0e57e81d3a07f02aca5a3978ccf57920
Adam Emery <aqemery@gmail.com>
2024-09-01 20:04:58 -0400

    correct image scrolling

---
+++ camera_2d.gd
@@ -0,0 +1,7 @@
+extends Camera2D
+
+func _input(event: InputEvent) -> void:
+    if event.is_action_pressed("show_minimap"):
+        anchor_mode = ANCHOR_MODE_DRAG_CENTER
+    elif event.is_action_released("show_minimap"):
+        anchor_mode = ANCHOR_MODE_FIXED_TOP_LEFT
