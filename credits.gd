9ee83c089f03c63482b5258dfc1ca86cb4cfa66e
Adam Emery <aqemery@gmail.com>
2024-10-14 16:38:19 -0400

    landing credits

--- credits.gd
+++ credits.gd
@@ -1,7 +1,9 @@
 extends Node2D
 
-#const ROOT = preload("res://root.tscn")
-# Called when the node enters the scene tree for the first time.
+@onready var label: Label = $Label
+var can_transition = false
+
+
 func _ready() -> void:
     pass # Replace with function body.
 
@@ -12,5 +14,14 @@ func _process(delta: float) -> void:
 
 # on event "fire" emit transistion to root scene
 func _input(event):
-    if event.is_action_pressed("fire"):
+    if event.is_action_released("fire") and can_transition:
         get_tree().change_scene_to_file("res://title.tscn")
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+    label.visible = true
+    can_transition = true
+    
+        
+    
+    
